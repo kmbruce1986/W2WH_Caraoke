e85bc2af1f646d42cacdd16def1c01cc98991b4d
kmbruce1986 <kmbruce1986@gmail.com>
2018-07-28 07:56:26 +0100

    adds rooms - count no of guests function

--- rooms.rb
+++ rooms.rb
@@ -2,9 +2,14 @@ class Rooms
 
 attr_reader :room_number
 
-def initialize(room_number)
+def initialize(room_number, guests, songs)
   @room_number = room_number
   @guests = []
-end 
+  @songs = []
+end
+
+def count_number_of_guests_in_room
+  return @guests.length
+end
 
 end
--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -2,16 +2,35 @@ require("minitest/autorun")
 require("minitest/rg")
 
 require_relative("../rooms.rb")
+require_relative("../guests.rb")
+require_relative("../songs.rb")
 
 class TestRooms < MiniTest::Test
 
   def setup
-    @room = Rooms.new(1)
+    @guest1 = Guests.new("Marvin")
+    @guest2 = Guests.new("Sirius")
+    @song1 = Songs.new("Let It go")
+    @song2 = Songs.new("Part Of Your World")
+    @guests = [@guest1, @guest2]
+    @songs = [@song1, @song2]
+    @room = Rooms.new(1, @guests, @songs)
   end
 
   def test_can_get_room_number
     assert_equal(1, @room.room_number)
   end
+
+  def test_count_number_of_guests_in_room
+    @room.count_number_of_guests_in_room
+    assert_equal(2, @guests.count)
+  end
+
+  # def test_can_check_in_guest
+  #   @room
+  #
+  #   assert_equal(2, @room.guests.count)
+  # end
 
 
 
