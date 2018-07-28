51fcad30a3c6058f35d94c1132ccf061a6c05448
kmbruce1986 <kmbruce1986@gmail.com>
2018-07-28 07:36:35 +0100

    adds rooms can get room number - test only

---
+++ specs/rooms_spec.rb
@@ -0,0 +1,18 @@
+require("minitest/autorun")
+require("minitest/rg")
+
+require_relative("../rooms.rb")
+
+class TestRooms < MiniTest:Test
+
+  def setup
+    @room = Rooms.new(1)
+  end
+
+  def test_can_get_room_number
+    assert_equal(1, @room.room_number)
+  end
+
+
+
+end
