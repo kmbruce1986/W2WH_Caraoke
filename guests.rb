ad94677f1be10b8e0c94adf9a0d2926938ac8bc2
kmbruce1986 <kmbruce1986@gmail.com>
2018-07-28 09:34:01 +0100

    addswallet attribute to Guests class and get wallet amount test - test passes

--- guests.rb
+++ guests.rb
@@ -1,10 +1,11 @@
 class Guests
 
-  attr_reader :guest_name
+  attr_reader :guest_name, :wallet
 
-  def initialize(guest_name)
+  def initialize(guest_name, wallet)
     @guest_name = guest_name
+    @wallet = wallet
   end
 
-  
+
 end
--- specs/guests_spec.rb
+++ specs/guests_spec.rb
@@ -6,11 +6,14 @@ require_relative("../guests.rb")
 class TestGuests < MiniTest::Test
 
   def setup
-    @guest = Guests.new("John")
+    @guest = Guests.new("John", 10)
   end
 
-def test_can_get_guest_name
-  assert_equal("John", @guest.guest_name)
-end
+  def test_can_get_guest_name
+    assert_equal("John", @guest.guest_name)
+  end
 
+  def test_can_get_amount_in_wallet
+    assert_equal(10, @guest.wallet)
+  end
 end
