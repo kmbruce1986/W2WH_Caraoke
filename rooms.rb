20d0e5a499e93d669f1f5011f2bdd216ca8fbf71
kmbruce1986 <kmbruce1986@gmail.com>
2018-07-29 15:36:25 +0100

    adds skill level removal and corrects previous counting issues - test passes

--- rooms.rb
+++ rooms.rb
@@ -10,12 +10,14 @@ class Rooms
     @till = till
   end
 
-  def count_number_of_guests_in_room
+  def guest_count
     return @guests.length
   end
 
   def remove_guest_from_room(guest)
-    @guests.delete(guest)
+    # @guests.delete(guest) - does this work????
+    index = @guests.index(guest).to_i
+    @guests.delete_at(index)
   end
 
   def add_song_to_room(song)
@@ -37,12 +39,17 @@ class Rooms
   def add_guest_to_room(guest, entry_fee)
     room_has_capacity()
     if room_has_capacity == true
-      @guests.push(guest)
+      @guests << guest
       check_favourite_song_on_playlist(guest)
       add_money_to_till(entry_fee)
       guest.remove_money_from_wallet(entry_fee)
     else return false
     end
+  end
+
+  def remove_guest_if_skill_level_drops_to_one
+    @guests.find { | guest | guest.skill_level == 1
+    remove_guest_from_room(guest)}
   end
 
 end
--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -8,12 +8,14 @@ require_relative("../songs.rb")
 class TestRooms < MiniTest::Test
 
   def setup
+    # I need to go over the setup of this.
     @guest1 = Guests.new("Marvin", 20, "Everybody Wants to be a Cat", 4)
     @guest2 = Guests.new("Sirius", 30, "A Friend in Me", 4)
-    @guest3 = Guests.new("Kathleen", 5, "When Will My Life Begin?", 5)
+    @guest3 = Guests.new("Kathleen", 5, "When Will My Life Begin?", 1)
+    @guest4 = Guests.new("Barry", 3, "I Want to build a snowman", 6)
     @song1 = Songs.new("Let It go")
     @song2 = Songs.new("Part Of Your World")
-    @guests = [@guest1, @guest2, @quest3]
+    @guests = [@guest1, @guest2, @guest3]
     @songs = [@song1, @song2]
     @room = Rooms.new(1, @guests, @songs, 3, 100)
   end
@@ -23,19 +25,26 @@ class TestRooms < MiniTest::Test
   end
 
   def test_count_number_of_guests_in_room
-    @room.count_number_of_guests_in_room
-    assert_equal(3, @guests.count)
+    @room.add_guest_to_room(@guest1, 5)
+    @room.add_guest_to_room(@guest2, 5)
+    @room.add_guest_to_room(@guest3, 5)
+    @room.guest_count
+    assert_equal(3, @room.guest_count)
   end
 
   def test_can_check_in_guest
-    @room.add_money_to_till(5)
-    assert_equal(3, @guests.count)
+    # @room.add_money_to_till(5)
+    @room.add_guest_to_room(@guest4, 5)
+    assert_equal(1, @room.guest_count)
     assert_equal(105, @room.till)
   end
 
   def test_can_check_out_guest
+    @room.add_guest_to_room(@guest1, 5)
+    @room.add_guest_to_room(@guest2, 5)
+    @room.add_guest_to_room(@guest3, 5)
     @room.remove_guest_from_room(@guest3)
-    assert_equal(3, @guests.count)
+    assert_equal(2, @room.guest_count)
   end
 
   def test_add_song_to_room
@@ -53,14 +62,14 @@ class TestRooms < MiniTest::Test
   end
 
   def test_guest_rejected_if_no_capacity
-    @guest4 = Guests.new("Barry", 3, "I Want to build a snowman", 6)
+
     # I'm not 100% why I need to define the entry_fee here - is there a more elegant way to do this? Should I add the entry fee to the Room class?
     entry_fee = 5
     @room.add_guest_to_room(@guest1, entry_fee)
     @room.add_guest_to_room(@guest2, entry_fee)
     @room.add_guest_to_room(@guest3, entry_fee)
     @room.add_guest_to_room(@guest4, entry_fee)
-    assert_equal(3, @guests.count)
+    assert_equal(4, @room.guest_count)
   end
 
   def test_can_get_amount_in_till
@@ -76,6 +85,14 @@ class TestRooms < MiniTest::Test
     @room.add_song_to_room("Everybody Wants to be a Cat")
     result = @room.check_favourite_song_on_playlist(@guest1)
     assert_equal("Woo", result)
+  end
+
+  def test_remove_guest_if_skill_level_drops_to_1
+    @room.add_guest_to_room(@guest1, 5)
+    @room.add_guest_to_room(@guest2, 5)
+    @room.add_guest_to_room(@guest3, 5)
+    @room.remove_guest_if_skill_level_drops_to_one
+    assert_equal(2, @room.guest_count)
   end
 
 
